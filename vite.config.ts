e179851efbc318d6e6d92e57aeb2ac4e9aa6e121
Techie Oriname <33547272+techieoriname@users.noreply.github.com>
2021-11-21 11:10:11 +0100

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,35 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        lib: {
+            name: 'Vue3PhoneInput',
+            entry: './src/js/index.ts',
+            formats: ['es', 'umd'],
+            fileName: (format) => {
+                let suffix: string = format;
+
+                if (format === 'es') {
+                    suffix = 'esm';
+                }
+                return `js/vue3-phone-input.${suffix}.js`;
+            }
+        },
+        emptyOutDir: false,
+        watch: {
+            include: 'src/**'
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                },
+                exports: 'named'
+            }
+        },
+        minify: false
+    },
     plugins: [vue()],
     resolve: {
         alias: {
